91f5b9bcddac037f8d2821afd9a1a4f8a9343d90
Shinrael <mircoelena89@gmail.com>
2024-05-09 17:51:23 +0200

    [fix] main

--- config/products.php
+++ config/products.php
@@ -3,8 +3,8 @@
 return [
     [
         "id"=> 1,
-        "frontImage"=> "../public/img/1.webp",
-        "backImage"=> "../public/img/1b.webp",
+        "frontImage"=> asset("img/1.webp"),
+        "backImage"=> asset("img/1b.webp"),
         "brand"=> "Levi's",
         "name"=> "Relaxed Fit",
         "price"=> 29.99,
@@ -22,8 +22,8 @@ return [
     ],
     [
         "id"=> 2,
-        "frontImage"=> "../public/img/2.webp",
-        "backImage"=> "../public/img/2b.webp",
+        "frontImage"=> asset("img/2.webp"),
+        "backImage"=> asset("img/2b.webp"),
         "brand"=> "Guess",
         "name"=> "Roses Tee",
         "price"=> 20.99,
@@ -37,8 +37,8 @@ return [
     ],
     [
         "id"=> 3,
-        "frontImage"=> "../public/img/3.webp",
-        "backImage"=> "../public/img/3b.webp",
+        "frontImage"=> asset("img/3.webp"),
+        "backImage"=> asset("img/3b.webp"),
         "brand"=> "Come Zucchero Filato",
         "name"=> "Voglia di colori pastello",
         "price"=> 129.99,
@@ -52,8 +52,8 @@ return [
     ],
     [
         "id"=> 4,
-        "frontImage"=> "../public/img/4.webp",
-        "backImage"=> "../public/img/4b.webp",
+        "frontImage"=> asset("img/4.webp"),
+        "backImage"=> asset("img/4b.webp"),
         "brand"=> "Levi's",
         "name"=> "Tee Unisex",
         "price"=> 14.99,
@@ -71,8 +71,8 @@ return [
     ],
     [
         "id"=> 5,
-        "frontImage"=> "../public/img/5.webp",
-        "backImage"=> "../public/img/5b.webp",
+        "frontImage"=> asset("img/5.webp"),
+        "backImage"=> asset("img/5b.webp"),
         "brand"=> "Maya Deluxe",
         "name"=> "Stripe Bodice",
         "price"=> 99.99,
@@ -90,8 +90,8 @@ return [
     ],
     [
         "id"=> 6,
-        "frontImage"=> "../public/img/6.webp",
-        "backImage"=> "../public/img/6b.webp",
+        "frontImage"=> asset("img/6.webp"),
+        "backImage"=> asset("img/6b.webp"),
         "brand"=> "Esprit",
         "name"=> "Maglione - Black",
         "price"=> 29.99,
